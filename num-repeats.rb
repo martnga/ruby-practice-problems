8610ab7d2bcb827e7dca82c788cf9afca160f4bf
mtnjoroge <mtnjoroge@gmail.com>
2015-08-13 16:14:25 +0300

    num-repeats.rb tests all true

---
+++ num-repeats.rb
@@ -0,0 +1,30 @@
+# Write a method that takes in a string and returns the number of
+# letters that appear more than once in the string. You may assume
+# the string contains only lowercase letters. Count the number of
+# letters that repeat, not the number of times they repeat in the
+# string.
+#
+
+
+def num_repeats(string)
+  i = 0
+  counter = 0
+  uniq_chars_array = string.chars.uniq
+  while i < uniq_chars_array.length
+    if string.chars.count(uniq_chars_array[i]) > 1
+    counter += 1
+    end
+    i += 1
+  end
+  counter
+end
+
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+
+puts('num_repeats("abdbc") == 1: ' + (num_repeats('abdbc') == 1).to_s)
+# one character is repeated
+puts('num_repeats("aaa") == 1: ' + (num_repeats('aaa') == 1).to_s)
+puts('num_repeats("abab") == 2: ' + (num_repeats('abab') == 2).to_s)
+puts('num_repeats("cadac") == 2: ' + (num_repeats('cadac') == 2).to_s)
+puts('num_repeats("abcde") == 0: ' + (num_repeats('abcde') == 0).to_s)
